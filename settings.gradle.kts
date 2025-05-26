f357bbf311a565fe85b51498b41b7d397ff88721
Eliezer Graber <me@eygraber.com>
2025-05-26 17:54:44 -0400

    Specify plugin repos in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,23 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+
+        maven("https://oss.sonatype.org/content/repositories/snapshots") {
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
+
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots") {
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
+
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "ffmpeg-cli-wrapper"
 
 plugins {
