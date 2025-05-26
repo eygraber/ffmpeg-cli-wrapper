4e5091b6b02c7eeeeb8f3b58f5e9e4277e4e2642
Eliezer Graber <me@eygraber.com>
2025-05-26 17:34:55 -0400

    Add develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "ffmpeg-cli-wrapper"
+
+plugins {
+    id("com.gradle.develocity") version "4.0.1"
+}
+
+val isCI = System.getenv("CI") == "true"
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        publishing.onlyIf { isCI }
+        if(isCI) {
+            termsOfUseAgree = "yes"
+        }
+    }
+}
