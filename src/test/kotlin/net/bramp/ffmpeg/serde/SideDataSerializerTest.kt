49e2849366c13b87cb387b9a5f98b825fe6f36aa
Eliezer Graber <me@eygraber.com>
2025-10-24 14:38:37 -0400

    Ensure test parity

---
+++ src/test/kotlin/net/bramp/ffmpeg/serde/SideDataSerializerTest.kt
@@ -0,0 +1,51 @@
+package net.bramp.ffmpeg.serde
+
+import kotlinx.serialization.json.Json
+import net.bramp.ffmpeg.probe.FFmpegStream
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SideDataSerializerTest {
+
+  private val json = Json { ignoreUnknownKeys = true }
+
+  @Test
+  fun testNormalVideo() {
+    val jsonString = """
+      {
+        "side_data_type": "Display Matrix",
+        "displaymatrix": "\n00000000:            0      -65536           0\n00000001:        65536           0           0\n00000002:            0           0  1073741824\n",
+        "rotation": 90
+      }
+    """.trimIndent()
+
+    val sideData = json.decodeFromString<FFmpegStream.SideData>(jsonString)
+
+    assertEquals("Display Matrix", sideData.sideDataType)
+    assertEquals(
+      "\n00000000:            0      -65536           0\n00000001:        65536           0           0\n00000002:            0           0  1073741824\n",
+      sideData.displayMatrix
+    )
+    assertEquals(90, sideData.rotation)
+  }
+
+  @Test
+  fun testZeroRotation() {
+    val jsonString = """
+      {
+        "side_data_type": "Display Matrix",
+        "displaymatrix": "\n00000000:            0           0           0\n00000001:            0           0           0\n00000002:            0           0  1073741824\n",
+        "rotation": 0
+      }
+    """.trimIndent()
+
+    val sideData = json.decodeFromString<FFmpegStream.SideData>(jsonString)
+
+    assertEquals("Display Matrix", sideData.sideDataType)
+    assertEquals(
+      "\n00000000:            0           0           0\n00000001:            0           0           0\n00000002:            0           0  1073741824\n",
+      sideData.displayMatrix
+    )
+    assertEquals(0, sideData.rotation)
+  }
+}
